34d7abb82edc7b19ae89a3cf5f6573c1bfdce0de
Joe Price <joseph.price@numbersplace.com>
2018-10-14 17:06:21 -0400

    Initial version from Andy

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IRobot.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode;
+
+public interface IRobot {
+    public void driveForward(double power, double timeMs);
+
+    public void driveBack(double power, double timeMs);
+
+    public void turnRight(double power, double timeMs);
+
+    public void turnLeft(double power, double timeMs);
+}
+
