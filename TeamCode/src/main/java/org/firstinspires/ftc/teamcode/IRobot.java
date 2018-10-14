9f6cbe3b387f68eee3d4fb9f2b6299746cd10328
Noah <noahcohen1000@gmail.com>
2018-10-14 17:21:28 -0400

    ,

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IRobot.java
@@ -1,12 +1,12 @@
 package org.firstinspires.ftc.teamcode;
 
 public interface IRobot {
-    public void driveForward(double power, double timeMs);
+    public void driveForward(double power, long timeMs);
 
-    public void driveBack(double power, double timeMs);
+    public void driveBack(double power, long timeMs);
 
-    public void turnRight(double power, double timeMs);
+    public void turnRight(double power, long timeMs);
 
-    public void turnLeft(double power, double timeMs);
+    public void turnLeft(double power, long timeMs);
 }
 
